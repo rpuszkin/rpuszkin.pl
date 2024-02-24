2c8c0a7cf0cb237bc6fe4f001341b86a7d9491f9
Robert Puszkin <rpuszkin@gmail.com>
2024-02-24 21:28:34 +0100

    error with border-left in support me was repaired, couple of paddings and margins was changed, other little miteakes was repaired

--- menu/supportme.php
+++ menu/supportme.php
@@ -1,6 +1,6 @@
 <section class="content-supportme" id="supportme">
 <h2><p class="subpage-title">wspomóż mnie</p></h2>
-<p>można mnie wspomóc mnie w rehabilitacji można następująco:</p>
-<div class="content"  id="fundation" ><i class="fa-solid fa-circle"></i> wWkonaj przelew  na moje konto w  <img src="media/menu-fundation.png"/> fundacji</div>
+<p>można mnie wspomóc mnie w rehabilitacji następująco:</p>
+<div class="content"  id="fundation" ><i class="fa-solid fa-circle"></i> Wykonaj przelew  na moje konto w  <img src="media/menu-fundation.png"/> fundacji</div>
 <div class="content" id="tax"><i class="fa-solid fa-circle"></i> Przekaż <img src="media/menu-tax.png" /> podatku</div>
 </section>
