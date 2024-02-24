1809b1d6d936f5cab9670646fc94099416a51f09
Robert Puszkin <rpuszkin@gmail.com>
2024-02-24 14:59:09 +0100

    sectins in supportme was turned into divs and recived huge left border

--- menu/supportme.php
+++ menu/supportme.php
@@ -1,6 +1,6 @@
 <section class="content-supportme" id="supportme">
 <h2><p class="subpage-title">wspomóż mnie</p></h2>
 <p>można mnie wspomóc mnie w rehabilitacji można następująco:</p>
-<section class="content"  id="fundation" ><i class="fa-solid fa-circle"></i> wWkonaj przelew  na moje konto w  <img src="media/menu-fundation.png"/> fundacji</section>
-<section class="content" id="tax"><i class="fa-solid fa-circle"></i> Przekaż <img src="media/menu-tax.png"/> podatku</section>
+<div class="content"  id="fundation" ><i class="fa-solid fa-circle"></i> wWkonaj przelew  na moje konto w  <img src="media/menu-fundation.png"/> fundacji</div>
+<div class="content" id="tax"><i class="fa-solid fa-circle"></i> Przekaż <img src="media/menu-tax.png" /> podatku</div>
 </section>
