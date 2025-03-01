ff16b7bd6d73a91d6babc44d66f8ebfd3f4f1fa9
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2025-03-01 19:48:27 +0100

    scrollIt() function was rebuilded - now delaywas added to give a time  for content loading

--- scroll.js
+++ scroll.js
@@ -1,44 +1,49 @@
 function scrollIt(targetId, duration) {
   return new Promise((resolve, reject) => {
     function attemptScroll(attemptsLeft) {
-      var target = document.getElementById(targetId);
+      setTimeout(() => {
+        var target = document.getElementById(targetId);
 
-      if (!target) {
-        if (attemptsLeft > 0) {
-          setTimeout(() => attemptScroll(attemptsLeft - 1), 100);
+        if (!target) {
+          if (attemptsLeft > 0) {
+            setTimeout(() => attemptScroll(attemptsLeft - 1), 100);
+            return;
+          }
+          reject("Target not found");
           return;
         }
-        reject(new Error("Target element not found"));
-        return;
-      }
 
-      var targetPosition = target.getBoundingClientRect().top;
-      var startPosition =
-        window.pageYOffset || document.documentElement.scrollTop;
-      var startTime = null;
+        requestAnimationFrame(() => {
+          var targetPosition =
+            target.getBoundingClientRect().top + window.pageYOffset;
+          var startPosition = window.pageYOffset;
+          var startTime = null;
 
-      function scrollToTarget(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = easeInOutQuad(Math.min(1, timeElapsed / duration));
-        var scrollPosition = startPosition + targetPosition * run;
-        window.scrollTo({
-          top: scrollPosition,
-          behavior: "auto",
-        });
+          function scrollToTarget(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            var timeElapsed = currentTime - startTime;
+            var run = easeInOutQuad(Math.min(1, timeElapsed / duration));
+            var scrollPosition =
+              startPosition + (targetPosition - startPosition) * run;
+            window.scrollTo({
+              top: scrollPosition,
+              behavior: "auto",
+            });
 
-        if (timeElapsed < duration) {
+            if (timeElapsed < duration) {
+              requestAnimationFrame(scrollToTarget);
+            } else {
+              resolve();
+            }
+          }
+
           requestAnimationFrame(scrollToTarget);
-        } else {
-          resolve();
-        }
-      }
+        });
+      }, 50);
+    }
 
-      function easeInOutQuad(t) {
-        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-      }
-
-      requestAnimationFrame(scrollToTarget);
+    function easeInOutQuad(t) {
+      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
     }
 
     attemptScroll(5);
