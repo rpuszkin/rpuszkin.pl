b62fc18c38810be8cf9919ec5b2b04f486b2d7c2
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2025-06-06 17:10:56 +0200

    changing time spent on contentwho decide about load smoothly

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function scrollAndLoad(menuLoad, subLoad) {
     }, 1000);
   }
   let secoundsOnContent = (Date.now() - window.startContentWatching) / 1000;
-  if ((window.contentInVP && secoundsOnContent < 4.5) || !window.contentInVP) {
+  if ((window.contentInVP && secoundsOnContent < 5.2) || !window.contentInVP) {
     if (window.contentInVP) {
       if (subLoad) {
         loadSmoothly(menuLoad, subLoad);
