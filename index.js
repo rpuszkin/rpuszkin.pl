150cdcf7c94c7ea3890a50d99c23cbc095294fba
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2025-11-02 14:34:06 +0100

    deleting stopScrollungNow befoe animation choosing

--- index.js
+++ index.js
@@ -169,7 +169,6 @@ function loadWithEffect(menuLoad, subLoad) {
       () => fade.forEach((el) => el.classList.remove("invisible")),
       2300
     );
-    window.hasAutoScrolled = true;
   }
   //type of transition to new content type choosing
   if (window.scrollY === 0) loadContent(menuLoad, subLoad);
