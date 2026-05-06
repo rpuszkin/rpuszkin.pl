fdfca1c75552dfcb835544a065388c5cf9981ffc
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2026-05-06 19:01:16 +0200

    fixing dropdown closing

--- index.js
+++ index.js
@@ -127,6 +127,8 @@ function loadContent(menuLoad, subLoad, contentPop) {
     );
 }
 function goTo(menuGo, subGo, popGo) {
+  const supportmeCheckbox = document.getElementById("supportme-checkbox");
+  if (supportmeCheckbox.checked && !subGo) supportmeCheckbox.checked = false;
   const fadeElements = document.querySelectorAll(".main, .header");
   function waitForTransitions(elements, { timeout = 700 } = {}) {
     return new Promise((resolve) => {
