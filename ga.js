0d5bc5d1bebb0844a2513a2e08fc7013147525f4
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2026-08-14 18:13:23 +0200

    blockig all GA cofig o local

--- ga.js
+++ ga.js
@@ -3,16 +3,15 @@ window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
-
-// Konfiguracja GA4 ze stemplem czasu
-gtag("js", new Date());
-
-// Rejestracja usługi z wyłączeniem automatycznego page_view na starcie
-gtag("config", "G-VF1R248K06", {
-  send_page_view: false,
-});
-
-// Funkcja wywoływana po podmianie zawartości w SPA
+if (
+  window.location.hostname !== "127.0.0.1" &&
+  window.location.hostname !== "localhost"
+) {
+  gtag("js", new Date());
+  gtag("config", "G-VF1R248K06", {
+    send_page_view: false,
+  });
+}
 function ga_script() {
   if (
     window.location.hostname === "127.0.0.1" ||
