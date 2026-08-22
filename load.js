446d4148b1f99e2185efe31f15f32995cdb8d8e0
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2026-08-22 16:08:49 +0200

    chenging it -loadContent() decide bout animation type by arguments not appStte - 404 bug

--- load.js
+++ load.js
@@ -185,11 +185,7 @@ function goTo(menuGo, subGo, popGo) {
       document.body.style.overflowY = "scroll";
   }
   //choosing type of transition to new content
-  if (
-    window.scrollY === 0 &&
-    popGo !== "pop" &&
-    window.appState.subOk !== "404"
-  )
+  if (window.scrollY === 0 && popGo !== "pop" && menuGo !== "404")
     return loadContent(menuGo, subGo, popGo);
   else return loadSmoothly(menuGo, subGo, popGo);
 }
