ab3c923a883151184b0a63b8589cc21451386e58
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2026-09-04 22:10:02 +0200

    adding new analytics g-tag

--- ga.js
+++ ga.js
@@ -1,33 +1,24 @@
-/**
- * Google Analytics 4 – własny skrypt page_view
- * Measurement ID: G-9KM42DQC1Y
- */
-
-// Inicjalizacja kolejki danych i funkcji pomocniczej gtag
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
 }
 
-// Inicjalizacja GA4 (poza localhost)
 if (
   window.location.hostname !== "127.0.0.1" &&
   window.location.hostname !== "localhost"
 ) {
   gtag("js", new Date());
-  gtag("config", "G-9KM42DQC1Y", {
-    send_page_view: false, // wyłączamy automatyczny page_view
+  gtag("config", "G-9NTHTCJ0M5", {
+    send_page_view: false,
   });
 }
 
-// Ręczne wysyłanie page_view
 function ga_script() {
   if (
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
   )
     return;
-
   if (typeof gtag === "function") {
     gtag("event", "page_view", {
       page_title: document.title,
